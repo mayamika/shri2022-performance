7f44d83039c31a2428c47446c3575e9987ca1dd6
mayamika <andranik.chakiryan@yandex.ru>
2022-08-26 19:11:49 +0300

    Move dist to docs dir

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   mode: 'production',
   entry: './scripts.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'docs'),
     filename: '[name].[contenthash].js',
     clean: true,
   },
